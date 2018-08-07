79d59bcd4207f2efbf7a79f265d6c74947e5cf7e
Achyuth <achyuth.kodali97@gmail.com>
2018-08-07 16:24:27 +0530

    7-8-18 16:24

--- python_django/urls.py
+++ python_django/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 from .views import *
 
@@ -28,3 +30,6 @@ urlpatterns = [
     url(r'^register_auth', check_register),
     url(r'^dashboard', dashboard)
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
